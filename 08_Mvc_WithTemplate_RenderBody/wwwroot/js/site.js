906419f57a9e099b991477c46f88dc904632dc1a
Elif Kaya <84388617+elifkaya3@users.noreply.github.com>
2022-04-06 10:28:15 +0300

    Add project files.

---
+++ 08_Mvc_WithTemplate_RenderBody/wwwroot/js/site.js
@@ -0,0 +1,44 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+/*const aktif = document.querySelector("#li");
+aktif.addEventListener("fullscreenchange", e => {
+    e.preventDefault();
+
+    )
+*/
+/*$(document).ready("onClick", (e) => {
+    {
+        e.preventDefault();
+
+       $("li").click(function () {
+    $("li").css("background-color", "red");
+})
+      
+    }
+})*/
+
+/*$("li").click(function () {
+    $("li").css("background-color", "red");
+})*/
+
+
+
+/*Hoca ile yapıldı ama forech sorun çıkardı. alttaki örnek çalışıyor 
+ * css yerine addclass kullanabiliriz içine oluşturacağımız yeni classı ekleyebilirz*/
+/*$(document).ready(function () {
+    $("li a").toArray().forEach(eleman => {
+        if (eleman.attr("href") == window.location.pathname)
+            $(this).addClass("active-link");
+    })
+})*/
+
+
+$(document).ready(function () {
+    $("li a").each(function () {
+        if ($(this).attr("href") == window.location.pathname)
+            $(this).css("background-color", "red");
+    })
+})
